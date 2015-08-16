410285519a46edc5fc9b3c975e16818c7d042e36
unknown <basti_12@gmx.de>
2015-08-16 23:46:53 +0200

    first commit

---
+++ src/main/java/org/swag/YoloBotApplication.java
@@ -0,0 +1,12 @@
+package org.swag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoloBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoloBotApplication.class, args);
+    }
+}
